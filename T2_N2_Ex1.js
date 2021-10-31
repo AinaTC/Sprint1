27714e21b5a0726d3ec56754a2583a01ba06b5f8
AinaTC <92716551+AinaTC@users.noreply.github.com>
2021-10-31 11:57:29 +0100

    Delete T2_N2_Ex1.js

--- T2_N2_Ex1.js
+++
@@ -1,5 +0,0 @@
-/* Crea una arrow function que, rebent un paràmetre, retorni un
- objecte amb un atribut que tingui com a valor el paràmetre rebut. */
-
-let p = par => obj = {atr:par};
-console.log(p("aa"));
