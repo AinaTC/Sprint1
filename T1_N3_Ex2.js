a0932384b3b39ecc48d94d0b6b54d3ac49ec4779
AinaTC <92716551+AinaTC@users.noreply.github.com>
2021-10-31 11:55:57 +0100

    Delete T1_N3_Ex2.js

--- T1_N3_Ex2.js
+++
@@ -1,5 +0,0 @@
-/*Crea una funció anònima autoinvocable igualada a una 
-variable que mostri per consola el nom de l'usuari rebut 
-com a paràmetre.
- */ 
-var ex2 = (function(NomUsuari){ console.log(NomUsuari); })("Usuari 3");
