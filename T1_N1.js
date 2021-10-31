9545be51259a1850aa333230ac7023ff19747424
AinaTC <92716551+AinaTC@users.noreply.github.com>
2021-10-31 11:54:59 +0100

    Delete T1_N1.js

--- T1_N1.js
+++
@@ -1,7 +0,0 @@
-/* Crea una funció que mostri per consola el nom d'usuari
- al invocar-la passant-li el nom com a paràmetre.*/
-function ex1(UserName){
-    console.log(UserName);
-};
-
-ex1("Usuari 1")
