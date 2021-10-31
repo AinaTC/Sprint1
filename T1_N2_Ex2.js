4bc4ff6a3f52ec5300ee201e41d8073edf659389
AinaTC <92716551+AinaTC@users.noreply.github.com>
2021-10-31 11:55:24 +0100

    Delete T1_N2_Ex2.js

--- T1_N2_Ex2.js
+++
@@ -1,9 +0,0 @@
-//Invoca una funció que retorni un valor mitjançant template literals.
-function ex2(Nom, Cognom){
-    let NomUsuari = Nom;
-    let CognomUsuari = Cognom;
-    console.log( `Nom:${NomUsuari}, Cognom:${CognomUsuari}`);
-}
-
-ex2("Aina", "Tubau");
-//module.exports = { ex2 };
